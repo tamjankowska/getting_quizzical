553ce6eb2fcff1e15fc7d85656d148cd9e8619ae
Tam Jankowska <tam.jankowska@gmail.com>
2020-12-07 11:23:44 +0000

    #3 index.js and router file

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const session = require("express-session");
+const app = express();
+const mongoose = require('mongoose');
+
+const indexRouter = require('./routes/indexRouter');
+
+require("dotenv").config();
+
+mongoose.connect(process.env.mongoConnectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const connection = mongoose.connection;
+
+connection.once('open', function () {
+    console.log('MongoDB database connection established successfully!');
+})
+
+app.use('/', indexRouter);
+
+app.use(cors());
+app.use(session({resave: true, saveUninitialized: true, secret: 'asdf'}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.listen(5000, () => {
+    console.log('App is online.');
+});
