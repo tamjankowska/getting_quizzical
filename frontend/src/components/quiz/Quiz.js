d949d38d2cd193efd895161eeff84633a68ec8e8
Tam Jankowska <tam.jankowska@gmail.com>
2020-12-09 22:37:57 +0000

    #19 quiz data now displays upon click
    Correct and incorrect answers are now shuffled together.

--- frontend/src/components/quiz/Quiz.js
+++ frontend/src/components/quiz/Quiz.js
@@ -1,58 +1,96 @@
 import React, { useEffect, useState } from "react";
 import "./Quiz.css";
-import axios from 'axios';
-import {nanoid} from 'nanoid';
+import axios from "axios";
+import { nanoid } from "nanoid";
 require("dotenv").config();
 
 function Quiz() {
-    const [questions, setQuestions] = useState([]);
-    const [categoryID] = React.useState(nanoid);
-    const [difficultyID] = React.useState(nanoid);
-    const [typeID] = React.useState(nanoid);
-    const [correctID] = React.useState(nanoid);
-    const [incorrectID] = React.useState(nanoid);
+  const [questions, setQuestions] = useState([]);
+  const [quizStarted, setQuizStarted] = useState(false);
 
+  const [questionID] = useState(nanoid);
+  const [categoryID] = useState(nanoid);
+  const [difficultyID] = useState(nanoid);
+  const [typeID] = useState(nanoid);
+  const [correctID] = useState(nanoid);
+  const [incorrectID] = useState(nanoid);
+  const [answersID] = useState(nanoid);
 
-    const getQuestions = async () => {
-        let url = `https://opentdb.com/api.php?amount=10`;
-        await axios.get(url).then((res) => {
-            setQuestions(res.data.results);   
-        });
-        
-    };
+  const getQuestions = async () => {
+    let url = `https://opentdb.com/api.php?amount=10`;
+    await axios.get(url).then((res) => {
+      setQuestions(res.data.results);
+    });
+  };
 
-    useEffect(() => {
-        if (questions.length == 0) {
-            getQuestions();
-        } else {
-            displayQuestions();
-            console.log(questions)
-        }
-    })
-
-    const displayQuestions = () => {
-
+  useEffect(() => {
+    if (questions.length == 0) {
+      getQuestions();
     }
+  });
 
-    return (
-        <div className="quizWrapper">
-            <h1>Quiz stuff</h1>
-            <div>
-            </div>
-            <div className = "quizData">
-                {questions.map((question) => (
-                    <div className = "quizItems">
-                        <h1 className = "category" key = {categoryID}>{question.category}</h1>
-                        <h1 className = "difficulty" key = {difficultyID}>{question.difficulty}</h1>
-                        <h1 className = "quizType" key = {typeID}>{question.type}</h1>
-                        <h1 className = "correctAnswer" key = {correctID}>{question.correct_answer}</h1>
-                        <h1 className = "incorrectAnswer" key = {incorrectID}>{question.incorrect_answer}</h1>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  const startQuiz = (event) => {
+    event.preventDefault();
+    event.target.style.display = "none";
+    setQuizStarted(true);
+  };
+
+  const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+
+  const startTimer = () => {
+    let currentTime = Date.now();
+    let interval = 1000; // ms
+    setInterval(function() {
+        let timeDiff = Math.floor((Date.now() - currentTime) / 1000);
+
+    })
+  };
+
+
+  return (
+    <div className="quizWrapper">
+      <div className="quizData">
+        <button id="startQuiz" value="startQuiz" onClick={startQuiz}>
+          Ready? Let's go!
+        </button>
+        {quizStarted ? (
+          <div>
+           
+            {questions.map((question) => (
+              <div className="quizItems">
+                <h1 className="question" key={questionID}>
+                  {question.question}
+                </h1>
+
+                <h2 className="category" key={categoryID}>
+                  {question.category}
+                </h2>
+
+                <h3 className="difficulty" key={difficultyID}>
+                  {question.difficulty}
+                </h3>
+
+                <h4 className="quizType" key={typeID}>
+                  {question.type}
+                </h4>
+
+                <h5 className="allAnswers" key={answersID}>
+                  {shuffle([
+                    question.correct_answer,
+                    ...question.incorrect_answers,
+                  ]).map((answer) => (
+                    <p className="answer">{answer}</p>
+                  ))}
+                </h5>
+              </div>
+            ))}
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Quiz;
-
