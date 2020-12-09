b757338fa1bc3057decd051bdd436be40e18d562
ecbaldocke612 <e.charlotte94@gmail.com>
2020-12-09 10:33:46 +0000

    Links from mainpage added

--- frontend/src/components/mainpage/MainpageSection.js
+++ frontend/src/components/mainpage/MainpageSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './MainpageSection.css';
+import { Link } from 'react-router-dom';
 
 
 function MainpageSection(props) { 
@@ -8,7 +9,7 @@ function MainpageSection(props) {
             {/* <img class="M-section" id="m-section-image" src={props.photo} /> */}
             <h2 className="M-section" id="M-section-h">{props.heading}</h2>
             <p className="M-section" id="M-section-p">{props.text}</p>
-            <button className="M-section" id="M-section-button">{props.button}</button>
+            <button className="M-section" id="M-section-button"><Link to={props.link}>{props.button}</Link></button>
         </div>
     )
 }
