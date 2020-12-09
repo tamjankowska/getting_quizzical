0e2567a29dc1d53a51569edc305c63e5784d4b9b
ecbaldocke612 <e.charlotte94@gmail.com>
2020-12-09 13:57:01 +0000

    ALtered cards to just have colors
    (some image options are commented out cause I can't get them to work just yet)

--- frontend/src/components/mainpage/MainpageSection.js
+++ frontend/src/components/mainpage/MainpageSection.js
@@ -3,14 +3,15 @@ import './MainpageSection.css';
 import { Link } from 'react-router-dom';
 
 
-function MainpageSection(props) { 
+function MainpageSection(props) {
     return (
-        <div className="M-section-container">
-            {/* <img class="M-section" id="m-section-image" src={props.photo} /> */}
-            <h2 className="M-section" id="M-section-h">{props.heading}</h2>
-            <p className="M-section" id="M-section-p">{props.text}</p>
-            <button className="M-section" id="M-section-button"><Link to={props.link}>{props.button}</Link></button>
-        </div>
+            <div className="M-section-container" >
+            {/* styles="background:url({{ props.url })" */}
+                <h2 className="M-section" id="M-section-h">{props.heading}</h2>
+                <p className="M-section" id="M-section-p">{props.text}</p>
+                <button className="M-section" id="M-section-button"><Link to={props.link}>{props.button}</Link></button>
+                {/* <img class="M-section" id="m-section-image" src={props.photo} /> */}
+            </div>
     )
 }
 
