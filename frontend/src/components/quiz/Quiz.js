97f87c8f2aca36c5ff8b5802d04c1d92454cc6ac
Tam Jankowska <tam.jankowska@gmail.com>
2020-12-14 13:33:17 +0000

    Updated gameplay

--- frontend/src/components/quiz/Quiz.js
+++ frontend/src/components/quiz/Quiz.js
@@ -12,7 +12,7 @@ function Quiz() {
   const [answersID] = useState(nanoid);
 
   const getQuestions = async () => {
-    let url = `https://opentdb.com/api.php?amount=10`;
+    let url = `https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple`;
     await axios.get(url).then((res) => {
       setQuestions(res.data.results);
     });
@@ -33,102 +33,117 @@ function Quiz() {
   const [index, setIndex] = useState(1);
 
   const shuffle = (array) => {
-        let shuffledArray = array.sort(() => Math.random() - 0.5);
-        return shuffledArray;  // need to add in if statement so that it only shuffles once when timeLeft === 30
-        // currently shuffles on every rerender 
-      
-  }
-  
+    let shuffledArray = array.sort(() => Math.random() - 0.5);
+    return shuffledArray; // need to add in if statement so that it only shuffles once when timeLeft === 30
+    // currently shuffles on every rerender
+  };
+
   const [timeLeft, setTimeLeft] = useState(30);
   const [points, setPoints] = useState(0);
 
   useEffect(() => {
-    if (timeLeft <= 30 && timeLeft >= 1) { 
+    if (timeLeft <= 30 && timeLeft >= 1) {
       const timer = setInterval(() => {
-        setTimeLeft(timeLeft - 1);   
+        setTimeLeft(timeLeft - 1);
       }, 1000);
       return () => clearInterval(timer);
-    } 
+    }
     if (timeLeft === 0) {
-        setIndex(index + 1);
-        return;
+      setIndex(index + 1);
+      return;
     }
   });
 
   const questionCorrect = async () => {
     if (timeLeft >= 0) {
-        setIndex(index + 1);
-        setPoints(points + 10);
-        {setTimeLeft(30)}
-        alert("Correct! Total points: " + (points + 10));
+      setIndex(index + 1);
+      setPoints(points + 10)
+      setTimeLeft(30);
+      
+      console.log(index + "Correct! Total points: " + (points + 10));
     }
-}  
+  };
 
-const questionIncorrect = () => {
+  const questionIncorrect = () => {
     if (timeLeft >= 0) {
-        setIndex(index + 1);
-        {setTimeLeft(30)}
-        console.log(index);
-        console.log("Incorrect. Total points: " + points)
+      setIndex(index + 1);
+      setTimeLeft(30);
+      
+      console.log(index);
+      console.log(index + "Incorrect. Total points: " + points);
     }
-}
+  };
 
-const playGame = () => {
-    return (    
-        <div className = "quizBeingPlayed">
-            {questions.slice((index - 1), index).map((question) => (
-              <div className="quizItems">
-                
-                <h1 className="question" key={questionID}>
-                  {question.question}
-                </h1>
+  const playGame = () => {
+    if (index === 11) {
+      setIndex(index + 1);
+      alert("How quizzical did you get?! Your total score is: " + points);
+      return;
+    }
 
-                    <h2 className="category-difficulty" key={categoryID}>
-                      {question.category} | {question.difficulty}
-                    </h2>
+    return (
+      <div className="quizBeingPlayed">
+        {questions.slice(index - 1, index).map((question) => (
+          <div className="quizItems">
+            <h1 className="question" key={questionID}>
+              {question.question}
+            </h1>
 
-                <div className="allAnswers" key={answersID}>
-                  {([ // shuffle has been removed from here, needs to be added back in when fixed
-                    question.correct_answer,
-                    ...question.incorrect_answers,
-                  ]).map((answer) => (
-                    <div className="radio__input">
-                      <input onClick = {() => {
-                          if (answer === question.correct_answer) {
-                              questionCorrect();
-                          } else if (answer !== question.correct_answer) {
-                              questionIncorrect();
-                              console.log("The correct answer was " + question.correct_answer)
-                          }
-                      }}
-                        key={answersID}
-                        type="radio"
-                        name="answer"
-                        defaultChecked={false}
-                        id={answersID}
-                        className="radio__answer"
-                      ></input>
-                      <label className="radio__label" htmlFor="radio1">
-                        {answer}
-                        <br />
-                      </label>
-             
-                    </div>
-                  ))}
-                  
+            <h2 className="category-difficulty" key={categoryID}>
+              {question.category} | {question.difficulty}
+            </h2>
+
+            <div className="allAnswers" key={answersID}>
+              {([
+                // shuffle has been removed from here, needs to be added back in when fixed
+                question.correct_answer,
+                ...question.incorrect_answers,
+              ]).map((answer) => (
+                <div className="radio__input">
+                  <input
+                    onClick={() => {
+                      if (answer === question.correct_answer) {
+                        questionCorrect();
+                        alert("Correct! Total points: " + (points + 10));
+                      } else if (answer !== question.correct_answer) {
+                        questionIncorrect();
+                        alert(
+                          "Incorrect. Total points: " +
+                            points +
+                            ". The correct answer was: " +
+                            question.correct_answer
+                        );
+                      }
+                    }}
+                    key={answersID}
+                    type="radio"
+                    name="answer"
+                    defaultChecked={false}
+                    id={answersID}
+                    className="radio__answer"
+                  ></input>
+                  <label className="radio__label" htmlFor="radio1">
+                    {answer}
+                    <br />
+                  </label>
                 </div>
-                <h1 className = "quiz-timeLeft">{timeLeft}</h1>
-                <button className = "quiz-nextQuestion" onClick = {() => {
-                    setIndex(index + 1);
-                    setTimeLeft(30);
-                }}>Next Question!</button>
-               
-              </div>
-            ))}
-            
+              ))}
+            </div>
+            <h1 className="quiz-timeLeft">{timeLeft}</h1>
+            <button
+              className="quiz-nextQuestion"
+              onClick={() => {
+                setIndex(index + 1);
+                setTimeLeft(30);
+              }}
+            >
+              Next Question!
+            </button>
           </div>
-    )
-}
+        ))}
+      </div>
+    );
+  };
   const replaceEntities = (html) => {
     let data = questions;
     questions.innerHTML = html;
@@ -141,7 +156,7 @@ const playGame = () => {
         <button id="startQuiz" value="startQuiz" onClick={startQuiz}>
           Ready? Let's get quizzical!
         </button>
-        {quizStarted ? (playGame()) : ("")}
+        {quizStarted ? playGame() : ""}
       </div>
     </div>
   );
